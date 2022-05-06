1f1c4f97541377ef6104a2dc764f8b6659dae03c
jaku14 <jameskuhn91@gmail.com>
2022-05-06 17:58:58 -0500

    message

--- src/tampa/Main.java
+++ src/tampa/Main.java
@@ -2,12 +2,15 @@ package tampa;
 
 import java.io.File;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+
+
         Scanner scan = new Scanner(System.in);
         int i;
 
@@ -24,13 +27,14 @@ public class Main {
 
         if (answer.equals("yes")) {
             System.out.println("SAVED");
+            meth2(i);
         } else if (answer.equals("no")) {
             System.out.println("NOT SAVED");
         } else {
                 System.out.println("please answer yes or no");
             }
 
-meth2();
+
 
 
     }
@@ -42,12 +46,20 @@ meth2();
 
     }
 
-    public static void meth2 ()   {
+    public static void meth2 (int i)   {
+
+
         try {
             File myObj = new File("C:\\Users\\after\\Downloads\\thefile.txt");
 
+
+
             if (myObj.createNewFile()) {
                 System.out.println("filecreated: " + myObj.getName());
+
+                FileWriter myWriter = new FileWriter("C:\\Users\\after\\Downloads\\thefile.txt");
+                myWriter.write(Integer.toString(i));
+                myWriter.close();
             }
         else{
                     System.out.println("file already exists.");
